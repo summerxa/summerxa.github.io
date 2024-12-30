092c52a71b6260b497e5e42c57b1c4229678b324
summerxa <anne.xia6@gmail.com>
2024-12-30 11:13:03 -0800

    update visuals, remove text label on projects

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+function buildGallery(images) {
+    const galleryContainer = document.getElementById("image-gallery");
+
+    images.forEach(image => {
+        const imgElement = document.createElement("img");
+        imgElement.src = image.src;
+        imgElement.alt = image.alt;
+        imgElement.classList.add("gallery-image");
+
+        galleryContainer.appendChild(imgElement);
+    });
+}
+
 function onGalleryClicked(url, caption) {
     // TODO show full image and caption
 }
